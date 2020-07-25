08a0b34b0b3ff7248f1cd296eb2da554c83748f0
betti <jsy950601@gmail.com>
2020-07-25 19:21:40 +0900

    나누어 떨어지는 수

--- programmers/src/level1/Main.java
+++ programmers/src/level1/Main.java
@@ -2,10 +2,17 @@ package level1;
 
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Solution sn = new Solution();
-		int [] arr = {1,1,3,3,0,1,1};
-		sn.solution(arr);
+//		// TODO Auto-generated method stub
+//		Solution sn = new Solution();
+//		int [] arr = {1,1,3,3,0,1,1};
+//		sn.solution(arr);
+		
+		Solution2 sol = new Solution2();
+		int [] arr = {2, 36, 1, 3};
+		int divisor = 1;
 				
+		sol.solution(arr, divisor);
+		
 	}
 }
+
