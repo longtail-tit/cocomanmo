fcdd1d2a1e0b61655a4fcaab13bfea13ce9bd9b6
betti <jsy950601@gmail.com>
2020-07-31 20:50:15 +0900

    수박수박수박수

--- programmers/src/level1/Main.java
+++ programmers/src/level1/Main.java
@@ -3,9 +3,9 @@ package level1;
 public class Main {
 	public static void main(String[] args) {
 		
-	   Solution4 sol = new Solution4();
-	   String s = "Pyy";
-	   sol.solution(s);
+	   Solution5 sol = new Solution5();
+	   int n = 4;
+	   System.out.println(sol.solution(n));
 	}
 }
 
